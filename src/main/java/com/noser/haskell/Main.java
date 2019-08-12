72b6a53a1312f0300b7c5625d92cbf2bc48bdf39
Thomas Chassaing <Thomas.Chassaing@noser.com>
2019-08-12 10:02:29 +0200

    Change Main

--- src/main/java/com/noser/haskell/Main.java
+++ src/main/java/com/noser/haskell/Main.java
@@ -8,12 +8,20 @@ class Main {
 
         Scheduler scheduler = new Scheduler("important-stuff");
 
-        scheduler.addTask("task1", 3, () -> {
-            System.out.print("dududu...");
+        scheduler.addTask("task1", 3L, () -> {
+            System.out.print(System.currentTimeMillis() + "-task1: de do do do...");
             return Unit.INSTANCE;
         });
-        scheduler.addTask("task2", 3, () -> {
-            System.out.print("dadada...");
+        scheduler.addTask("task2", 3L, () -> {
+            System.out.print(System.currentTimeMillis() + "-task2: de da da da...");
+            return Unit.INSTANCE;
+        });
+        scheduler.addTask("task3", 3L, () -> {
+            System.out.print(System.currentTimeMillis() + "-task3: is all I want...");
+            return Unit.INSTANCE;
+        });
+        scheduler.addTask("task4", 3L, () -> {
+            System.out.print(System.currentTimeMillis() + "-task4: to say to you...");
             return Unit.INSTANCE;
         });
 
