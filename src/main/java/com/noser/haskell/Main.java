a42eeb8ce8dd263eac9f4cb46c12bd1ea605bf4d
Thomas Chassaing <thomas.chassaing@noser.com>
2019-08-29 17:54:10 +0200

    Print add message

--- src/main/java/com/noser/haskell/Main.java
+++ src/main/java/com/noser/haskell/Main.java
@@ -1,6 +1,7 @@
 package com.noser.haskell;
 
 import kotlin.Unit;
+import kotlin.jvm.functions.Function1;
 
 class Main {
 
@@ -8,22 +9,27 @@ class Main {
 
         Scheduler scheduler = new Scheduler("important-stuff");
 
+        Function1<Task,Unit> printer = (task) -> {
+            System.out.println(task + " added");
+            return Unit.INSTANCE;
+        };
+
         scheduler.addTask("task1", 3L, () -> {
             System.out.print(System.currentTimeMillis() + "-task1: de do do do...");
             return Unit.INSTANCE;
-        });
+        }).forEach(printer);
         scheduler.addTask("task2", 3L, () -> {
             System.out.print(System.currentTimeMillis() + "-task2: de da da da...");
             return Unit.INSTANCE;
-        });
+        }).forEach(printer);
         scheduler.addTask("task3", 3L, () -> {
             System.out.print(System.currentTimeMillis() + "-task3: is all I want...");
             return Unit.INSTANCE;
-        });
+        }).forEach(printer);
         scheduler.addTask("task4", 3L, () -> {
             System.out.print(System.currentTimeMillis() + "-task4: to say to you...");
             return Unit.INSTANCE;
-        });
+        }).forEach(printer);
 
         try {
             Thread.sleep(16_000);
